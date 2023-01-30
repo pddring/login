22811b35574d851d1fbd430ca7bb7169445eb31f
fulfordcs <fulfordcs@gmail.com>
2023-01-30 11:18:26 +0000

    Client side validation

---
+++ Server/Server/static/lib.js
@@ -0,0 +1,35 @@
+const eHints = document.getElementById("hints");
+
+function reset() {
+    
+}
+
+function createUser() {
+    
+}
+
+function login() {
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    try {
+        presenceCheck(username, "Please enter your username");
+        presenceCheck(password, "Please enter your password");
+        lengthCheck(password, 5, 10, "Please enter a password between 5 and 10 characters");
+        eHints.innerText = "Attempting login";
+    } catch (e) {
+        eHints.innerText = e;
+    }
+    
+}
+
+function lengthCheck(input, minLength, maxLength, message) {
+    if (input.length > maxLength || input.length < minLength) {
+        throw message;
+    }
+}
+
+function presenceCheck(input, message) {
+    if (input == "") {
+        throw message;
+    } 
+}
